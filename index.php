4e3aa73a588d49b03b3ab909e27bffe3e8e6b43d
tiakjoshii <tilakjoshibaitadi@gmail.com>
2023-11-13 19:12:04 +0545

    change login page

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
- require_once 'database_connection_create.php';
+require_once 'database_connection_create.php';
 session_start();
 
 if (isset($_POST['user'])) {
@@ -21,12 +21,10 @@ if (isset($_POST['user'])) {
         } else {
             // echo "enter correct password";
             header('location:index.php?error=enter correct password');
-
         }
     } else {
         // echo "enter correct email address";
         header('location:index.php?error=enter correct email address');
-
     }
 
     $connect->close();
@@ -39,8 +37,10 @@ if (isset($_POST['user'])) {
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style1.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css" integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <!-- <link rel="stylesheet" href="style1.css"> -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css"
+        integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
     <title>Login_Form</title>
     <style>
         .divpass {
@@ -50,22 +50,28 @@ if (isset($_POST['user'])) {
 
         .spart {
             position: absolute;
-            left: 320px;
-            top: 13px;
-            z-index: 1;
+            right: 38px;
+            top: 15px;
+            z-index: 100;
 
 
+        }
+
+        .spart i {
+            font-size: 1.2rem;
         }
 
         .open {
             display: none;
         }
     </style>
+    <link rel="stylesheet" href="./style11.css">
+
 
 </head>
 
 <body>
-    <div class="row">
+    <!-- <div class="row">
         <div class="facebook-text">
             <h1>SNSC Student</h1>
             <p>Connect with our group or the college <br> around you on campus.</p>
@@ -80,12 +86,12 @@ if (isset($_POST['user'])) {
                 </div>
                 <button class="button" type="submit">log in</button> <br>
                 <?php
-            if (isset($_GET['error'])) {
-                echo '<div style="color:red; padding-bottom: .5rem;" >' . $_GET['error'] . '</div>';
-            } elseif(isset($_GET['success'])) {
-                echo '<div style="color:green;">' . $_GET['success'] . '</div>';
-            }
-            ?>
+                // if (isset($_GET['error'])) {
+                //     echo '<div style="color:red; padding-bottom: .5rem;" >' . $_GET['error'] . '</div>';
+                // } elseif(isset($_GET['success'])) {
+                //     echo '<div style="color:green;">' . $_GET['success'] . '</div>';
+                // }
+                ?>
                 <a href="forget_password.php">forget password?</a>
             </form>
             <div class="create">
@@ -98,7 +104,87 @@ if (isset($_POST['user'])) {
             </div>
         </div>
 
-    </div>
+    </div> -->
+    <section> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span>
+        <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span>
+        <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span>
+        <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span>
+        <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span>
+        <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span>
+        <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span>
+        <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span>
+        <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span>
+        <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span>
+        <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span>
+        <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span>
+        <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span>
+        <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span>
+        <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span>
+        <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span>
+        <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span>
+        <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span>
+        <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span>
+        <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span>
+        <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span>
+        <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span>
+        <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span>
+        <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span>
+        <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span>
+        <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span>
+        <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span>
+        <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span>
+        <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span>
+        <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span>
+        <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span>
+        <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span>
+        <span></span> <span></span> <span></span> <span></span> <span></span>
+
+        <form action="#" method="post" class="signin">
+
+            <div class="content">
+
+                <h2>Sign In</h2>
+
+                <div class="form">
+
+                    <div class="inputBox">
+
+                        <input name="user" type="username" required> <i class="i">Username</i>
+
+                    </div>
+
+                    <div class="inputBox">
+
+                        <input id="pass" name="pass" type="password" required> <i class="i">Password</i>
+                        <small class="spart" onclick="funclick1()"><i id="hide1" class="fa-regular fa-eye open"></i>
+                            <i id="hide2" class="fa-regular fa-eye-slash close"></i></small>
+                    </div>
+                    <?php
+                    if (isset($_GET['error'])) {
+                        echo '<div style="color:red; padding-bottom: .5rem;" >' . $_GET['error'] . '</div>';
+                    } elseif (isset($_GET['success'])) {
+                        echo '<div style="color:green;">' . $_GET['success'] . '</div>';
+                    }
+                    ?>
+                    <div class="links"> <a href="forget_password.php">Forgot Password</a> <a
+                            href="form_sample_for_new_user.php">Signup</a>
+
+                    </div>
+
+                    <div class="inputBox">
+
+                        <input type="submit" value="Login">
+
+                    </div>
+
+                </div>
+
+            </div>
+
+        </form>
+
+    </section> <!-- partial -->
+
 </body>
 <script>
     function funclick1() {
